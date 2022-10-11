66934ab834d29d1d2a43e3e5861c47a5b49d322b
Udit Soni <91983775+udit6023@users.noreply.github.com>
2022-10-11 22:29:07 +0530

    permission commit

--- lib/bluePage.dart
+++ lib/bluePage.dart
@@ -6,6 +6,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_blue/flutter_blue.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class BluePage extends StatefulWidget {
   BluePage({Key? key, required this.title}) : super(key: key);
@@ -19,12 +21,11 @@ final List<ScanResult> devicesList = <ScanResult>[];
 }
  FlutterBlue flutterBlue = FlutterBlue.instance;
 class _BluePageState extends State<BluePage> {
-String? powerValue;
-
-
+late double powerValue;
    @override
  void initState() {
    super.initState();
+   
    widget.flutterBlue.scanResults
    .listen((List<ScanResult> devices) {
      for (ScanResult device in devices) {
@@ -36,7 +37,22 @@ String? powerValue;
        addDeviceToList(result);
      }
    });
-   widget.flutterBlue.startScan();
+ widget.flutterBlue.startScan();
+
+ new Timer.periodic(Duration (seconds: 5), (Timer t) => setState(() {
+    widget.flutterBlue.stopScan();
+     Navigator.pushReplacement(context, MaterialPageRoute(builder: (BuildContext context) => BluePage(title: 'bluE')));
+     Fluttertoast.showToast(  
+        msg: 'Refreshing...',  
+        toastLength: Toast.LENGTH_SHORT,    
+        gravity: ToastGravity.BOTTOM,  
+        timeInSecForIosWeb: 1,  
+        backgroundColor: Colors.black,  
+        textColor: Colors.white 
+    );
+     }));
+
+
  }
 
   @override
@@ -52,24 +68,25 @@ String? powerValue;
   addDeviceToList(final ScanResult device) {
    if (!widget.devicesList.contains(device)) {
      setState(() {
+     
        widget.devicesList.add(device);
      });
    }
+   
  }
+ 
 
   ListView viewDevices() {
+   
    List<Container> containers = <Container>[];
    for (ScanResult device in widget.devicesList) {
-    if(device.advertisementData.txPowerLevel.toString()!='null')
-    {
-      powerValue=((pow(10, (device.advertisementData.txPowerLevel!-device.rssi)/30).round())).toString();
-    }else{
-      powerValue="0";
-    }
-    setState(() {
-      powerValue;
-    });
+     powerValue=(double.parse(((pow(10, (-69-device.rssi)/30))).toStringAsFixed(5))) as double;
+   
+setState(() {
+     powerValue;
+     });
      containers.add(
+      
        Container(
          height: 100,
          width: 100,
@@ -81,6 +98,7 @@ String? powerValue;
                 
                  child: Column(
                    children:[
+                    
                     SizedBox(height: 20,),
                      Text(device.advertisementData.localName== '' ? '(unknown device)' : device.advertisementData.localName),
                  Text("Distance from beacon: "),
@@ -101,6 +119,8 @@ String? powerValue;
        ...containers,
      ],
    );
+
+
  }
   
 }
